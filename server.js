afd353605c6e026396a0b57dd6655f4bf3ea083c
Ajit Jadhav <ajitopensource@gmail.com>
2018-02-22 17:02:42 +0530

    Changed view engine from hbs to ejs

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const hbs = require('hbs');
+const ejs = require('ejs');
 const fs = require('fs');
 const mysql = require('./database/mysql');
 const routes = require('./routes/index');
@@ -7,9 +7,7 @@ const routes = require('./routes/index');
 var app = express();
 
 var con = mysql.initialize();
-
-hbs.registerPartials(__dirname + '/views/partials')
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 
 
 app.use('/', routes);
@@ -28,16 +26,6 @@ app.use((req, res, next) => {
 // });
 
 app.use(express.static(__dirname + '/public'));
-
-hbs.registerHelper('getCurrentYear', () => {
-  return new Date().getFullYear();
-});
-
-hbs.registerHelper('screamIt', (text) => {
-  return text.toUpperCase();
-});
-
-
 
 app.listen(3000, () => {
   console.log('Server is up on port 3000');
